dd060e959c878e58467cc33dfcc66485a5cc7ff1
Giuseppe Davide Salvatore Pomo <giuseppepomo8@gmail.com>
2024-11-18 17:14:01 +0100

    secondo commit giuseppe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
+//Globali pagina welcome
 const form = document.getElementById("footerForm");
 const btnWelcome = document.getElementById("welcomeBtn");
 const checkBox = document.getElementById("welcomeCheckBox");
 const advert = document.getElementById("advert");
 
+//Globali pagina questions
+const timer = document.getElementById ("timer")
+const questions = document.getElementById ("questions")
+const answers = document.getElementsByClassName ("answers")
+const counter = document.getElementById ("counter")
+
+
+// Funzioni pagina welcome
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     if(!checkBox.checked){
@@ -11,3 +20,6 @@ form.addEventListener("submit", function (e) {
     }
     form.submit();
 })
+
+//Funzioni pagina questions
+
