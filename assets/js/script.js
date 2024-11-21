e5523063034d25cc42ccf5dced7e1057efb9372b
Domy15 <domenico2016dellaventura@gmail.com>
2024-11-21 12:36:51 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById("footerForm");
-const btnWelcome = document.getElementById("welcomeBtn");
 const checkBox = document.getElementById("welcomeCheckBox");
 const advert = document.getElementById("advert");
 
