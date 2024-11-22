57ac032d7c35c8c2b5e9421581fd212a645f8bd8
DanieleGuicciardi <dany.guicciardi@outlook.com>
2024-11-22 09:51:27 +0100

    epicodeLogo>

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const form = document.getElementById("footerForm");
 const checkBox = document.getElementById("welcomeCheckBox");
 const advert = document.getElementById("advert");
+const logo = document.getElementById("headerLogo");
 
 //funzione evento che ti obbliga a fare check sul quadratino per proseguire
 form.addEventListener("submit", function (e) {
@@ -12,3 +13,7 @@ form.addEventListener("submit", function (e) {
     }
     form.submit(); //esegue l'evento
 })
+
+logo.addEventListener("click", function() {
+    alert("Hey dove pensi di andare? FAI IL TEST!");
+});
